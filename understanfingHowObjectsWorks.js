1637c9906ad0a24e587f0fc2b8183498d20cd580
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-06-15 18:20:01 -0300

    add metodos ligar e desligar

--- understanfingHowObjectsWorks.js
+++ understanfingHowObjectsWorks.js
@@ -12,9 +12,19 @@ class Carro {
     ligar() {
         if (!this.ligado) {
             this.ligado = true;
-            console.log(`O ${this.modelo} ${this.marca} ligou`);
+            console.log(`O ${this.modelo} ${this.marca} ligou.`);
         } else {
-            console.log(`O ${this.modelo} ${this.marca} ja esta ligado`);
+            console.log(`O ${this.modelo} ${this.marca} ja esta ligado.`);
+        }
+    }
+
+    desligar() {
+        if (this.ligado) {
+            this.ligado = false;
+            this.velocidadeAtual = 0;
+            console.log(`O ${this.modelo} ${this.marca} desligou.`)            
+        } else {
+            console.log(`O ${this.modelo} ${this.marca} já está ligado.`)            
         }
     }
 }
@@ -24,4 +34,5 @@ const carroCarlos = new Carro ("Chevrolet", "S10", "branco", 2025);
 
 console.log("\n\n Ações do carro do Carlos");
 
+carroCarlos.desligar();
 carroCarlos.ligar();
