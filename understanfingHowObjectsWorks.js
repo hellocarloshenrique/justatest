4ef93f39fa1a9d4fdbd9436609dce23352705e8f
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-06-15 19:40:59 -0300

    add o metodo acelerar

--- understanfingHowObjectsWorks.js
+++ understanfingHowObjectsWorks.js
@@ -5,7 +5,7 @@ class Carro {
         this.cor = cor;
         this.ano = ano;
         this.velocidadeAtual = 0;
-        this.logado = false;
+        this.ligado = false;
     }
 
     //criando os metodos que o carro pode fazer
@@ -22,17 +22,35 @@ class Carro {
         if (this.ligado) {
             this.ligado = false;
             this.velocidadeAtual = 0;
-            console.log(`O ${this.modelo} ${this.marca} desligou.`)            
+            console.log(`O ${this.modelo} ${this.marca} desligou.`);           
         } else {
-            console.log(`O ${this.modelo} ${this.marca} já está ligado.`)            
+            console.log(`O ${this.modelo} ${this.marca} já está ligado.`);           
         }
     }
-}
+
+    acelerar(quantidade) {
+        if (this.ligado) {
+            this.velocidadeAtual += quantidade;
+            console.log(`O ${this.modelo} ${this.marca} acelerou para ${this.velocidadeAtual} Km/h.`);
+        } else {
+            console.log(`O ${this.modelo} ${this.marca} está desligado. Não pode acelerar.`);
+        }
+    }
+} 
 
 // crinado as instancias/objetos
 const carroCarlos = new Carro ("Chevrolet", "S10", "branco", 2025);
+const carroBeth = new Carro ("Fiat", "Toro", "Preto", 2025);
 
 console.log("\n\n Ações do carro do Carlos");
 
 carroCarlos.desligar();
 carroCarlos.ligar();
+carroCarlos.acelerar(80);
+
+console.log("\n\n Ações do carro da Beth");
+
+carroBeth.ligar();
+carroBeth.desligar();
+carroBeth.ligar();
+carroBeth.acelerar(20);
