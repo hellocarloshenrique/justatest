7e26b69ad3e9d47e75a378446228aee7260821cc
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-06-15 17:28:52 -0300

    criando a receita... ou class

---
+++ understanfingHowObjectsWorks.js
@@ -0,0 +1,11 @@
+class Carro {
+    constructor(marca, modelo, cor, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ano = ano;
+        this.velocidadeAtual = 0;
+        this.logado = false;
+        
+    }
+}
